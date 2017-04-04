8e29d70de78c69316d0f63fb23e266cfb05fd105
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-04-04 20:35:02 +0000

    Remove amunmt code

--- moses/FF/NMT/common/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <boost/algorithm/string.hpp>
-
-void Trim(std::string& s);
-
-void Split(const std::string& line, std::vector<std::string>& pieces, const std::string del=" ");
--- moses/FF/NMT/vocab2txt.py
+++
@@ -1,8 +0,0 @@
-import sys
-import cPickle
-import operator
-
-d = cPickle.load(open(sys.argv[1], 'r'))
-sorted_d = sorted(d.items(), key=operator.itemgetter(1))
-for p in sorted_d:
-    print p[0]
